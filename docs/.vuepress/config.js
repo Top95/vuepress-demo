7ca27f93fd9b7801a3f42afcd7848bcb80a56ba3
top <1915145111@qq.com>
2022-03-27 20:45:08 +0800

    提交代码

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '你好啊 打工人',
+  description: 'Just do it！',
+  themeConfig: {
+    prevLinks: true,
+    nextLinks: true,
+    sidebar: 'auto',
+    logo: '/ths3.png',
+    nav: require("./nav.js"),
+    sidebar: require("./sidebar.js")
+  }
+}
